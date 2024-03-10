87dcc1edc7701a73cf65602850475c8fc0828c33
rmirma <115686470+rmirma@users.noreply.github.com>
2024-03-10 12:16:31 +0200

    Initial commit

---
+++ src/main/java/com/example/fujitsutrial/FujitsuTrialApplication.java
@@ -0,0 +1,13 @@
+package com.example.fujitsutrial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FujitsuTrialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FujitsuTrialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fujitsutrial/FujitsuTrialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fujitsutrial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FujitsuTrialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
